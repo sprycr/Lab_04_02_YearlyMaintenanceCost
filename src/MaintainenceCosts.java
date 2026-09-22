9f5f4231c484f8c76a86a1d704c33b5db6337bab
sprycr <sprycr@mail.uc.edu>
2026-09-21 23:45:12 -0400

    Completed task 2

---
+++ src/MaintainenceCosts.java
@@ -0,0 +1,13 @@
+public class MaintainenceCosts {
+    public static void main(String[] args) {
+        double springCosts = 3392.95;
+        double summerCosts = 3441.12;
+        double fallCosts = 3303.01;
+        double winterCosts = 3249.73;
+        double totalCosts;
+
+        totalCosts = springCosts+summerCosts+fallCosts+winterCosts;
+
+        System.out.println("the total cost for the whole year is: $" + totalCosts);
+    }
+}
